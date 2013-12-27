97b2e17984d795b209566cbac9606175bc9233f7
bdickason <dickason@gmail.com>
2013-12-26 22:52:25 -0500

    Updated test runnre to remove extranous shit

--- tests/test_runner.cpp
+++ tests/test_runner.cpp
@@ -1,12 +1,8 @@
-#include <iostream>
 #include "gtest/gtest.h"
 
 using namespace std;
 
-// int Factorial (int n);  // Returns the factorial of n
-
 int main(int argc, char **argv) {
-  cout << "test\n";  
 
   testing::InitGoogleTest(&argc, argv);
   return(RUN_ALL_TESTS());
--- tests/world_spec.cpp
+++ tests/world_spec.cpp
@@ -1,8 +1,15 @@
 #include "gtest/gtest.h"
+#include "../core/world.h"
 
+// Generic test to make sure tests are working
 TEST(firstTest, abs) {
-  EXPECT_EQ(1, abs(-1));
-  EXPECT_EQ(1, abs(1));
+    EXPECT_EQ(1, abs(-1));
+    EXPECT_EQ(1, abs(1));
+}
 
-  /* Insert Tests here! */
+TEST(worldConstructor, World) {
+    World *a = new World;
+
+    tmp = a->getHero();
+    EXPECT_EQ(tmp, "H");
 }
