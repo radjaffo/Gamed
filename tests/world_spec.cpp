85b50a4cedc2e1ba2a9851c55eedb51395fe9fd8
bdickason <dickason@gmail.com>
2013-12-26 22:52:59 -0500

    First (nonworking) test

--- tests/world_spec.cpp
+++ tests/world_spec.cpp
@@ -1,5 +1,8 @@
 #include "gtest/gtest.h"
 #include "../core/world.h"
+#include <iostream>
+
+using namespace std;
 
 // Generic test to make sure tests are working
 TEST(firstTest, abs) {
@@ -11,5 +14,6 @@ TEST(worldConstructor, World) {
     World *a = new World;
 
     tmp = a->getHero();
+
     EXPECT_EQ(tmp, "H");
 }
