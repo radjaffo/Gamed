abb592d95a17a3557993454747610bb02cc1390a
radjaffo <dickasongaming@gmail.com>
2013-12-26 22:25:15 -0600

    Added setID tests

--- tests/character_spec.cpp
+++ tests/character_spec.cpp
@@ -13,7 +13,30 @@ using namespace std;
 TEST(Character, getId) {
     Character *a = new Character;
 
+
     int tmp = a->getID();	//expected result 0
 
     EXPECT_EQ(tmp, 0); 
+}
+
+TEST(Character, setId)
+{
+	Character *b = new Character;
+
+	int i = 3;
+	int j = 1;
+	int c = 10000;
+
+	b->setID(i);		//expected result 3
+
+	EXPECT_EQ(b->getID(), i);
+
+	b->setID(j);		//expected result 1
+
+	EXPECT_EQ(b->getID(), j);
+	
+	b->setID(c);		//expected result 10000
+
+	EXPECT_EQ(b->getID(), c);
+
 }
